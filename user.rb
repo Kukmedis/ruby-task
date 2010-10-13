059eb1e1e52cb73df898e475f84c20ebd9b4a497
Jonas <jonas@inventi.lt>
2010-10-14 01:07:59 +0300

    Padariau is viso 5 testukus Useriui ir 1 Playeriui

--- user.rb
+++ user.rb
@@ -1,2 +1,10 @@
 class User
+  attr_accessor :money, :username, :password, :email
+  
+  def initialize(username, password, email)
+    @username = username
+    @password = password
+    @email = email
+    @money = 100
+  end
 end
--- user_spec.rb
+++ user_spec.rb
@@ -1,4 +1,26 @@
 require 'user'
 
-describe User do
+describe User, "When created" do
+  user = User.new("usern","passw","e@mai.l")
+  
+  it "should have 100 money" do    
+    user.money.should equal 100
+  end
+  
+  it "should have a username" do
+    user.username.should_not equal ""
+  end
+  
+  it "should have a password" do
+    user.password.should_not equal ""
+  end
+  
+  it "should have an email" do
+    user.email.should_not equal ""
+  end
+  
+  it "should be able too see its money" do
+    lambda {user.money}.should_not raise_error 
+  end
+  
 end
